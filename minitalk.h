50403d63534b8bbe0ce83a98fbd151fe7797e533
Levon Tumanyan <letumany@c1r4s3.42yerevan.am>
2022-02-08 19:57:15 +0400

    start 2f

---
+++ minitalk.h
@@ -0,0 +1,10 @@
+#ifndef MINITALK_H
+#define MINITALK_H
+
+#include<signal.h>
+#include<unistd.h>
+#include <stdlib.h>
+
+char		*ft_itoa(int n);
+
+#endif
--- server.c
+++ server.c
@@ -1,55 +1,6 @@
-#include<stdio.h>
-#include<signal.h>
-#include<unistd.h>
-#include <stdlib.h>
+#include "minitalk.h"
+#include <stdio.h>
 
-static int	nbrlen(int nbr)
-{
-	int i;
-
-	i = (nbr <= 0) ? 1 : 0;
-	if (nbr < 0)
-		nbr *= -1;
-	while (nbr)
-	{
-		nbr /= 10;
-		++i;
-	}
-	return (i);
-}
-
-char		*ft_itoa(int n)
-{
-	char	*str;
-	long	nbr;
-	int		len;
-
-	nbr = n;
-	len = nbrlen(nbr);
-	if (!(str = malloc(sizeof(unsigned char) * (len + 1))))
-		return (NULL);
-	if (nbr < 0)
-	{
-		nbr *= -1;
-		str[0] = '-';
-	}
-	if (nbr == 0)
-		str[0] = '0';
-	str[len--] = '\0';
-	while (nbr)
-	{
-		str[len] = (nbr % 10) + '0';
-		nbr /= 10;
-		--len;
-	}
-	return (str);
-}
-
-void sig_handler(int signum)
-{
-
-  printf("\nInside handler function\n");
-}
 void  ft_putstr(char *c)
 {
   int i = 0;
@@ -60,20 +11,25 @@ void  ft_putstr(char *c)
   }
 }
 
+void sig_handler(int signum)
+{
+
+  ft_putstr("\nInside handler function\n");
+}
 
 int main()
 {
   int pid;
+	struct sigaction sa;
 
+	sa.sa_handler = sig_handler;
+	sa.sa_flags = SA_SIGINFO;
   pid = (int)getpid();
 
-  ft_putstr(ft_itoa(pid));
 
-  printf("\n%d\n", pid);
-  signal(SIGINT,sig_handler); // Register signal handler
-  for(int i=1;;i++){    //Infinite loop
-    printf("%d : Inside main function\n",i);//
-    sleep(1);
-  }
+  ft_putstr(ft_itoa(pid));
+	sigaction(SIGUSR1, &sa, NULL); 
+
+  while (1);
   return 0;
 }
